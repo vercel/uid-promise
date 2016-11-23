da6d07c9bcd07078abcea1d5e682582de30b5efc
Mario Colque <colquemario@gmail.com>
2016-11-23 17:31:37 -0300

    added dashes `-_` to complete 64 chars

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ const crypto = require('crypto')
 // charset from github.com/coreh/uid2
 const UIDCHARS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
 
-module.exports = function (len) {
+// dashes
+const DASHES = '-_'
+
+module.exports = (len, useDashes = true) => {
   return new Promise((resolve, reject) => {
     if (typeof len !== 'number') {
       reject(new TypeError('You must supply a length integer to `uid-promise`.'))
       return
     }
+
+    const CHARS = useDashes ? UIDCHARS + DASHES : UIDCHARS
 
     crypto.randomBytes(len, (err, buf) => {
       if (err) {
@@ -16,7 +21,7 @@ module.exports = function (len) {
       }
       const str = []
       for (let i = 0; i < buf.length; i++) {
-        str.push(UIDCHARS[buf[i] % UIDCHARS.length])
+        str.push(CHARS[buf[i] % CHARS.length])
       }
       resolve(str.join(''))
     })
