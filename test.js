c2da84a65230b3e156119474f6307acd7a510867
Mario Colque <colquemario@gmail.com>
2016-11-23 17:31:58 -0300

    update example

--- test.js
+++ test.js
@@ -1,5 +1,9 @@
 const uid = require('.')
 
 uid(20).then(str => {
-  console.log('random uid:', str)
+  console.log('random uid with dashes:', str)
+})
+
+uid(20, false).then(str => {
+  console.log('random uid without dashes:', str)
 })
