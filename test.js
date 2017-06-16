82547e08ac5e01fbb93c3a3ed824e8a7b38be24c
/ᐠ｡ꞈ｡ᐟ <ricky.miller@gmail.com>
2017-06-16 13:43:42 -0400

    Add ava tests (#21)
    * Comment should be capitalized (according to xo)
    
    * Add ava to package.json
    
    * Add single test
    
    * Add ava to test script
    
    * Add chars.js
    
    * Add uid should contain only UIDCHARS

--- test.js
+++ test.js
@@ -1,5 +1,14 @@
+const test = require('ava')
+const UIDCHARS = require('./chars')
 const uid = require('.')
 
-uid(20).then(str => {
-  console.log('random uid:', str)
+test('uid should be correct length', async t => {
+  const len = Math.floor(Math.random() * 1000)
+  const u = await uid(len)
+  t.is(u.length, len)
+})
+
+test('uid should contain only UIDCHARS', async t => {
+  const u = await uid(20)
+  u.split('').forEach(l => t.not(UIDCHARS.indexOf(l), -1))
 })
